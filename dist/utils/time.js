88980c2b0a792ce9dcba6db26127c24bef4c3d30
Maksudur7 <maksudurrahamanmishu7@gmail.com>
2026-07-22 09:01:14 -0700

    fix: remove dist directory from git tracking and make requireEmailVerification configurable

--- dist/utils/time.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secondsToReadable = secondsToReadable;
-function secondsToReadable(totalSeconds) {
-    const sec = Number(totalSeconds || 0);
-    const hours = Math.floor(sec / 3600);
-    const minutes = Math.floor((sec % 3600) / 60);
-    return `${hours}h ${minutes}m`;
-}
